78be3a3ec7a70aceafafbadab070990d09030f48
AlexV <asmodehn@gmail.com>
2014-08-04 16:49:32 +0900

    fixing android build after directory restructure

--- Android.mk
+++ Android.mk
@@ -5,16 +5,16 @@ LOCAL_MODULE    := jansson_static
 
 LOCAL_MODULE_FILENAME := libjansson
 
-LOCAL_SRC_FILES := dump.c \
-    error.c \
-    hashtable.c \
-    load.c \
-    memory.c \
-    pack_unpack.c \
-    strbuffer.c \
-    strconv.c \
-    utf.c \
-    value.c
+LOCAL_SRC_FILES := src/dump.c \
+    src/error.c \
+    src/hashtable.c \
+    src/load.c \
+    src/memory.c \
+    src/pack_unpack.c \
+    src/strbuffer.c \
+    src/strconv.c \
+    src/utf.c \
+    src/value.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include $(LOCAL_PATH)/src
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
